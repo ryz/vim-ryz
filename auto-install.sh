674b4da15a0873fd6c3cbbf5acc6f60881ca8071
Steve Haßenpflug <ryzawy@gmail.com>
2012-04-23 18:52:22 +0200

    added auto install script

---
+++ auto-install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+VIMHOMEDIR=~/.vim
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+echo "Installing ryz' Vim configuration..."
+echo
+# check if git is installed
+command -v git >/dev/null 2>&1 || { echo >&2 "git is required, but not found! Aborting."; exit 1; }
+
+# check if Vim/vimrc already exists
+[ -e "$VIMHOMEDIR/vimrc" ] && die "$VIMHOMEDIR/vimrc already exists."
+[ -e "~/.vim" ] && die "~/.vim already exists."
+[ -e "~/.vimrc" ] && die "~/.vimrc already exists."
+
+
+cd ~
+git clone --recursive http://github.com/ryz/vim-ryz.git ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
+
+echo "ryz' .vim was successfully installed and configured, have fun!"
+
+# deprecated, only needed before Git version 1.6.5
+# cd ~/.vim
+# git submodule update --init
